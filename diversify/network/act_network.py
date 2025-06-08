1fe0d701ed42c888ea51abdc463d879fefb09f73
UCIHARadded <deadliestrishabh@gmail.com>
2025-06-08 17:56:37 +0530

    Update act_network.py

--- diversify/network/act_network.py
+++ diversify/network/act_network.py
@@ -1,20 +1,18 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+import torch.nn as nn
 import torch
 
-import torch.nn as nn
 
 var_size = {
     'emg': {
-        'in_size': 8,   # 8 channels
-        'ker_size': 9,
-        'input_len': 32*44
+        'in_size': 8,
+        'ker_size': 9
     },
     'uci_har': {
-        'in_size': 9,
-        'ker_size': 9,
-        'input_len': 32*30
+        'in_size': 570,  # 561 (features) + 9 (inertial)
+        'ker_size': 9
     }
 }
 
@@ -24,26 +22,30 @@ class ActNetwork(nn.Module):
         super(ActNetwork, self).__init__()
         self.taskname = taskname
         in_ch = var_size[taskname]['in_size']
-        ker_size = var_size[taskname]['ker_size']
+        kernel = var_size[taskname]['ker_size']
 
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=in_ch, out_channels=16, kernel_size=(1, ker_size)),
+            nn.Conv2d(in_channels=in_ch, out_channels=16, kernel_size=(1, kernel)),
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=(1, 2), stride=2)
         )
+
         self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(1, ker_size)),  # Changed here
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(1, kernel)),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=(1, 2), stride=2)
         )
+
+        # Init with dummy input to get correct flattened size
         with torch.no_grad():
-            dummy_input = torch.zeros(1, in_ch, 1, var_size[taskname]['input_len'])
-            dummy_out = self.conv2(self.conv1(dummy_input))
-            self.in_features = dummy_out.view(1, -1).size(1)
+            dummy = torch.zeros(1, in_ch, 1, 128)
+            out = self.conv2(self.conv1(dummy))
+            self.in_features = out.view(1, -1).shape[1]
 
     def forward(self, x):
-        x = self.conv2(self.conv1(x))
+        x = self.conv1(x)
+        x = self.conv2(x)
         x = x.view(-1, self.in_features)
         return x
