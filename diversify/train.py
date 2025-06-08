b43249d76c781f3491d0a9e025569ae276dffa30
UCIHARadded <deadliestrishabh@gmail.com>
2025-06-08 13:59:06 +0530

    Update train.py

--- diversify/train.py
+++ diversify/train.py
@@ -2,16 +2,22 @@
 # Licensed under the MIT License.
 
 import time
+import sys
+sys.path.append(".")  # Ensure current dir is included
+
 from alg.opt import *
 from alg import alg, modelopera
-from utils.util import set_random_seed, get_args, print_row, print_args, train_valid_target_eval_names, alg_loss_dict, print_environ
+from utils.util import (
+    set_random_seed,
+    get_args,
+    print_row,
+    print_args,
+    train_valid_target_eval_names,
+    alg_loss_dict,
+    print_environ
+)
 from datautil.getdataloader_single import get_act_dataloader
-import sys
-sys.path.append(".")  # Force look in current directory
-from uci_loader import get_uci_har_dataloader
-
-
-
+from uci_loader import get_uci_har_dataloader  # UCI loader
 
 def main(args):
     s = print_args(args, [])
@@ -19,19 +25,26 @@ def main(args):
 
     print_environ()
     print(s)
-    if args.latent_domain_num < 6:
-        args.batch_size = 32*args.latent_domain_num
-    else:
-        args.batch_size = 16*args.latent_domain_num
 
+    # UCI HAR-specific setting
+    if args.dataset == 'uci_har':
+        print("[INFO] Using UCI HAR dataset loader")
+        args.latent_domain_num = 1  # dummy value; HAR has no real domains
+
+    if args.latent_domain_num < 6:
+        args.batch_size = 32 * args.latent_domain_num
+    else:
+        args.batch_size = 16 * args.latent_domain_num
+
+    # Dataset loading
     if args.dataset == 'uci_har':
         train_loader, train_loader_noshuffle, valid_loader, target_loader, _, _, _ = get_uci_har_dataloader(args)
     else:
         train_loader, train_loader_noshuffle, valid_loader, target_loader, _, _, _ = get_act_dataloader(args)
 
-
     best_valid_acc, target_acc = 0, 0
 
+    # Algorithm
     algorithm_class = alg.get_algorithm_class(args.algorithm)
     algorithm = algorithm_class(args).cuda()
     algorithm.train()
@@ -43,33 +56,32 @@ def main(args):
         print(f'\n========ROUND {round}========')
         print('====Feature update====')
         loss_list = ['class']
-        print_row(['epoch']+[item+'_loss' for item in loss_list], colwidth=15)
+        print_row(['epoch'] + [item + '_loss' for item in loss_list], colwidth=15)
 
         for step in range(args.local_epoch):
             for data in train_loader:
                 loss_result_dict = algorithm.update_a(data, opta)
-            print_row([step]+[loss_result_dict[item]
-                              for item in loss_list], colwidth=15)
+            print_row([step] + [loss_result_dict[item] for item in loss_list], colwidth=15)
 
         print('====Latent domain characterization====')
         loss_list = ['total', 'dis', 'ent']
-        print_row(['epoch']+[item+'_loss' for item in loss_list], colwidth=15)
+        print_row(['epoch'] + [item + '_loss' for item in loss_list], colwidth=15)
 
         for step in range(args.local_epoch):
             for data in train_loader:
                 loss_result_dict = algorithm.update_d(data, optd)
-            print_row([step]+[loss_result_dict[item]
-                              for item in loss_list], colwidth=15)
+            print_row([step] + [loss_result_dict[item] for item in loss_list], colwidth=15)
 
-        algorithm.set_dlabel(train_loader)
+        # Only for EMG-type domain-aware datasets
+        if args.dataset != 'uci_har':
+            algorithm.set_dlabel(train_loader)
 
         print('====Domain-invariant feature learning====')
-
         loss_list = alg_loss_dict(args)
         eval_dict = train_valid_target_eval_names(args)
         print_key = ['epoch']
-        print_key.extend([item+'_loss' for item in loss_list])
-        print_key.extend([item+'_acc' for item in eval_dict.keys()])
+        print_key.extend([item + '_loss' for item in loss_list])
+        print_key.extend([item + '_acc' for item in eval_dict.keys()])
         print_key.append('total_cost_time')
         print_row(print_key, colwidth=15)
 
@@ -81,22 +93,20 @@ def main(args):
             results = {
                 'epoch': step,
             }
-
-            results['train_acc'] = modelopera.accuracy(
-                algorithm, train_loader_noshuffle, None)
-
+            results['train_acc'] = modelopera.accuracy(algorithm, train_loader_noshuffle, None)
             acc = modelopera.accuracy(algorithm, valid_loader, None)
             results['valid_acc'] = acc
-
             acc = modelopera.accuracy(algorithm, target_loader, None)
             results['target_acc'] = acc
 
             for key in loss_list:
-                results[key+'_loss'] = step_vals[key]
+                results[key + '_loss'] = step_vals[key]
+
             if results['valid_acc'] > best_valid_acc:
                 best_valid_acc = results['valid_acc']
                 target_acc = results['target_acc']
-            results['total_cost_time'] = time.time()-sss
+
+            results['total_cost_time'] = time.time() - sss
             print_row([results[key] for key in print_key], colwidth=15)
 
     print(f'Target acc: {target_acc:.4f}')
