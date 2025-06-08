03606f4e6ca510e859b2f077665124429b41ebb9
UCIHARadded <deadliestrishabh@gmail.com>
2025-06-08 12:59:49 +0530

    Update train.py

--- diversify/train.py
+++ diversify/train.py
@@ -6,7 +6,10 @@ from alg.opt import *
 from alg import alg, modelopera
 from utils.util import set_random_seed, get_args, print_row, print_args, train_valid_target_eval_names, alg_loss_dict, print_environ
 from datautil.getdataloader_single import get_act_dataloader
+import sys
+sys.path.append(".")  # Force look in current directory
 from uci_har_loader import get_uci_har_dataloader
+
 
 
 
