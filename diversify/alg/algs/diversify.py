32d0efd84c178ad7dc5ee4b5e9615172cf0655fe
UCIHARadded <deadliestrishabh@gmail.com>
2025-06-18 13:23:53 +0530

    Update diversify.py

--- diversify/alg/algs/diversify.py
+++ diversify/alg/algs/diversify.py
@@ -37,12 +37,22 @@ class Diversify(Algorithm):
         self.args = args
         self.dclassifier_initialized = False
 
-    def _initialize_dclassifier(self, z1):
+    # UPDATED _initialize_dclassifier method to accept a dataloader instead of tensor
+    def _initialize_dclassifier(self, loader):
         if not self.dclassifier_initialized:
-            z1_dim = z1.shape[1]
-            self.dclassifier = common_network.feat_classifier(z1_dim, self.args.latent_domain_num).cuda()
-            self.dclassifier_initialized = True
-            print(f"[INIT] dclassifier initialized with input dim: {z1_dim}")
+            self.featurizer.eval()
+            self.dbottleneck.eval()
+            with torch.no_grad():
+                for batch in loader:
+                    x = batch[0].cuda().float()  # assuming batch = (inputs, labels, domains, ...)
+                    z1 = self.dbottleneck(self.featurizer(x))
+                    z1_dim = z1.shape[1]
+                    self.dclassifier = common_network.feat_classifier(z1_dim, self.args.latent_domain_num).cuda()
+                    self.dclassifier_initialized = True
+                    print(f"[INIT] dclassifier initialized with input dim: {z1_dim}")
+                    break  # only need one batch to initialize
+            self.featurizer.train()
+            self.dbottleneck.train()
 
     def update_d(self, minibatch, opt):
         all_x1 = minibatch[0].cuda().float()
@@ -50,7 +60,9 @@ class Diversify(Algorithm):
         all_d1 = minibatch[2].cuda().long() % self.args.latent_domain_num
 
         z1 = self.dbottleneck(self.featurizer(all_x1))
-        self._initialize_dclassifier(z1)
+        # initialize dclassifier if not done yet
+        if not self.dclassifier_initialized:
+            self._initialize_dclassifier_from_tensor(z1)  # Remove this or change to always call with loader
 
         assert z1.shape[1] == self.dclassifier.fc.in_features, f"Shape mismatch: z1={z1.shape[1]} vs fc.in={self.dclassifier.fc.in_features}"
 
@@ -71,71 +83,6 @@ class Diversify(Algorithm):
         opt.step()
 
         return {'total': loss.item(), 'dis': disc_loss.item(), 'ent': ent_loss.item()}
-
-    def set_dlabel(self, loader):
-        self.dbottleneck.eval()
-        self.dclassifier.eval()
-        self.featurizer.eval()
-
-        start_test = True
-        with torch.no_grad():
-            iter_test = iter(loader)
-            for _ in range(len(loader)):
-                data = next(iter_test)
-                inputs = data[0].cuda().float()
-                index = data[-1]
-                feas = self.dbottleneck(self.featurizer(inputs))
-                outputs = self.dclassifier(feas)
-                if start_test:
-                    all_fea = feas.float().cpu()
-                    all_output = outputs.float().cpu()
-                    all_index = index
-                    start_test = False
-                else:
-                    all_fea = torch.cat((all_fea, feas.float().cpu()), 0)
-                    all_output = torch.cat((all_output, outputs.float().cpu()), 0)
-                    all_index = np.hstack((all_index, index))
-
-        all_output = nn.Softmax(dim=1)(all_output)
-        all_fea = torch.cat((all_fea, torch.ones(all_fea.size(0), 1)), 1)
-        all_fea = (all_fea.t() / torch.norm(all_fea, p=2, dim=1)).t().cpu().numpy()
-
-        K = all_output.size(1)
-        aff = all_output.numpy()
-        initc = aff.T.dot(all_fea) / (1e-8 + aff.sum(axis=0)[:, None])
-        dd = cdist(all_fea, initc, 'cosine')
-        pred_label = dd.argmin(axis=1)
-
-        for _ in range(1):
-            aff = np.eye(K)[pred_label]
-            initc = aff.T.dot(all_fea) / (1e-8 + aff.sum(axis=0)[:, None])
-            dd = cdist(all_fea, initc, 'cosine')
-            pred_label = dd.argmin(axis=1)
-
-        loader.dataset.set_labels_by_index(pred_label, all_index, 'pdlabel')
-        print(Counter(pred_label))
-        self.dbottleneck.train()
-        self.dclassifier.train()
-        self.featurizer.train()
-
-    def update(self, data, opt):
-        all_x = data[0].cuda().float()
-        all_y = data[1].cuda().long()
-        all_d = data[2].cuda().long()
-
-        all_z = self.bottleneck(self.featurizer(all_x))
-        disc_input = Adver_network.ReverseLayerF.apply(all_z, self.args.alpha)
-        disc_out = self.discriminator(disc_input)
-
-        disc_loss = F.cross_entropy(disc_out, all_d)
-        all_preds = self.classifier(all_z)
-        classifier_loss = F.cross_entropy(all_preds, all_y)
-
-        loss = classifier_loss + disc_loss
-        opt.zero_grad()
-        loss.backward()
-        opt.step()
-        return {'total': loss.item(), 'class': classifier_loss.item(), 'dis': disc_loss.item()}
 
     def update_a(self, minibatches, opt):
         all_x = minibatches[0].cuda().float()
